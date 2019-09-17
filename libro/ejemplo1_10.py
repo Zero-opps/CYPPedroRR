f18fc24e5abcdb75748b23150d019b1309cf60c5
Clank-09 <Zero-opps>
2019-09-17 17:56:40 -0500

    Se termina la prueba

---
+++ libro/ejemplo1_10.py
@@ -0,0 +1,5 @@
+NUM=int(input("Dame la base de un rectangulo"))
+BUM=int(input("Dame la altura de un rectangulo"))
+AR= NUM*BUM
+PR= 2(NUM) + 2(BUM)
+print(f"El area del rectangulo es {AR} y su perimetro {PR}")
