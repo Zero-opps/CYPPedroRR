b39870904c0f88fc0708aba5126fef2e2e917cb6
Clank-09 <Zero-opps>
2019-09-17 17:57:10 -0500

    Se termina la prueba

---
+++ ejemplo1_12.py
@@ -0,0 +1,5 @@
+A = float(input("Introduce un valor"))
+B = float(input("Dame otro nùmero"))
+RES = (A+B) ** (2/3)
+print (RES)
+       
---
+++ print.py
@@ -0,0 +1,34 @@
+# print tiene cuatro formas de us
+"""
+1.- con comas
+2.- con signo '+'
+3.- con la funcion format ()
+4.- Es con una variante de format ()
+"""
+# con comas
+# concatenar agregando un espacio y haciendo casting tipo
+edad = 10
+nombre = "Juan"
+estatura= 1.67
+
+print(edad, estatura, nombre)
+
+#con signo + hace lo mismo pero no realiza el casting automàtico
+#no agrega espacios
+
+print(str(edad) + str(estatura) + nombre)
+
+# funcion format()
+
+print("Nombre: {1} Edad: {0}".format(nombre, edad, estatura))
+
+# 4.- Es con una variante de format () simplificada
+
+print(f"Nombre: \"{nombre}\" \nEdad:\t{edad}")
+
+# Print caracter de escape diagonal invertida, si se ejecuta significa salto de linea, diagonal t
+# \" caracter de escape se usa para ingresar texto que no pertence al còdigo
+# \n \.
+# print y el argumento end
+print("Solo hay 10 tipos de personas las que saben binario y las que no",end="---")
+print("otra linea")
