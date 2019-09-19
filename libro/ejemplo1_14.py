3a81d4c7fc02eafe67b311f1a924b60379618c8b
Clank-09 <Zero-opps>
2019-09-19 15:45:35 -0500

    nice

---
+++ libro/ejemplo1_14.py
@@ -0,0 +1,4 @@
+a=int(input("Ingresa un número: "))
+CUA= a*a
+CUB= a**3
+print(f"El cuadra de a es {CUA} y el cubo {CUB} ")
---
+++ libro/ejemplo1_15.py
@@ -0,0 +1,4 @@
+a=int(input("Dame el precio del producto: "))
+b=int(input("Ingresa el efectivo: "))
+c=b-a
+print(f"Tu cambio sería: {c}") 
