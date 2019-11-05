006269ee0f4fa2b8e626cc0210e636a97adbadf6
Zero-opps <Zero-opps>
2019-11-05 17:45:49 -0600

    Trabajo de listas

---
+++ tuplas.py
@@ -0,0 +1,9 @@
+#Tuplas
+#se declaran con "("y")"
+#Son inmutlables y organizan dartos igual que las listas
+
+datos_db=('132.248.173.88','root','12345678')
+print(datos_db[1])
+
+#datos_db[1]='admin'
+#datos_db=('132.248.173.88','admin','12345678')
